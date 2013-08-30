501ddbade78175e65a676ef332760807fb33af5f
Alexander V. Ignatyev <ialx84@ya.ru>
2013-08-31 01:48:07 +0600

    Update tests

--- tests/test_type_ipv4.py
+++ tests/test_type_ipv4.py
@@ -7,7 +7,7 @@ import unittest
 class TestIPv4(unittest.TestCase):
     orig_ip_str = "127.0.0.1"
     orig_ip_list = [127, 0, 0, 1]
-    orig_ip_int  = 0
+    orig_ip_int  = 2130706433
 
     def setUp(self):
         print "TTT"
@@ -19,6 +19,19 @@ class TestIPv4(unittest.TestCase):
         self.assertEqual(str(self.ip_str), self.orig_ip_str)
         self.assertEqual(list(self.ip_str), self.orig_ip_list)
         self.assertEqual(int(self.ip_str), self.orig_ip_int)
+
+    def testExceptions(self):
+        # ipv4 accepts str, list and int
+        self.assertRaises(TypeError, ipv4, {"foo": "bar"})
+
+        # test validators
+        self.assertRaises(ValueError, ipv4, "foobar")    # non dotted decimal
+        self.assertRaises(ValueError, ipv4, "1.1.1.1.1")    # 5 octets
+        self.assertRaises(ValueError, ipv4, [444,444,444,444])  # 444 > 255
+        self.assertRaises(ValueError, ipv4, [1,1,1,1,1])    # 5 octets
+        self.assertRaises(ValueError, ipv4, 99999999999)    # very big int
+
+
 
 if __name__ == "__main__":
     unittest.main()
